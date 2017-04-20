cd5d57a7d73a5e38080cc4ee4a285c318a321821
alexsmithmarist <alex.smith1@marist.edu>
2017-04-20 14:34:47 -0400

    Adding files for Lab 6

---
+++ labs/6/Lab6_problem91.java
@@ -0,0 +1,41 @@
+/**
+ * File : Lab6_problem91.java
+ * Assignment : Lab 6
+ * Author : Alex Smith
+ * Course : CMPT220
+ * Due Date : April 20, 2017
+ * Version : 1.0
+ */
+
+public class Lab6_problem91{
+
+  public static class Rectangle{
+    double width = 1;
+    double height = 1;
+
+    Rectangle(){
+    }
+
+    Rectangle(double setwidth, double setheight){
+      height = setheight;
+      width = setwidth;
+    }
+
+    double getArea(){
+      return width * height;
+    }
+
+    double getPerimeter() {
+      return (width + width + height + height);
+    }
+  }
+
+  public static void main(String[] args){
+    Rectangle rectangle1 = new Rectangle(4, 40);
+    Rectangle rectangle2 = new Rectangle(3.5, 35.9);
+
+    System.out.println("rectangle1 : Width = "+rectangle1.width+", Height = "+rectangle1.height+", Area = "+rectangle1.getArea()+", Perimeter = "+rectangle1.getPerimeter());
+    System.out.println("rectangle2 : Width = "+rectangle2.width+", Height = "+rectangle2.height+", Area = "+rectangle2.getArea()+", Perimeter = "+rectangle2.getPerimeter());
+  }
+
+}
---
+++ labs/6/Lab6_problem913.java
@@ -0,0 +1,68 @@
+/**
+ * File : Lab6_problem913.java
+ * Assignment : Lab 6
+ * Author : Alex Smith
+ * Course : CMPT220
+ * Due Date : April 20, 2017
+ * Version : 1.0
+ */
+import java.util.Scanner;
+
+public class Lab6_problem913{
+
+  public static class Location{
+    public int row = 0;
+    public int column = 0;
+    public double maxValue = 0;
+
+    Location(){
+    }
+
+    Location(int setrow, int setcolumn){
+      row = setrow;
+      column = setcolumn;
+    }
+
+    double[] locateLargest(double[][] a){
+      
+      for(int crow = 0; crow < a.length; crow++){
+        for(int ccolumn = 0; ccolumn < a[crow].length; ccolumn++){
+          if(maxValue < a[crow][ccolumn]){
+            maxValue = a[crow][ccolumn];
+            row = crow;
+            column = ccolumn;
+          }
+        }
+      }
+
+      double[] maxRowAndColumn = new double [3];
+      maxRowAndColumn[0] = row;
+      maxRowAndColumn[1] = column;
+      maxRowAndColumn[2] = maxValue;
+
+      return maxRowAndColumn;
+    }
+
+  }
+
+  public static void main(String[] args){
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter the number of rows and columns in the array: ");
+    int rows = input.nextInt();
+    int columns = input.nextInt();
+
+    double[][] twoDArray = new double [rows][columns];
+    System.out.println("Enter the array: ");
+    for(int row = 0; row < twoDArray.length; row++){
+      for(int column = 0; column < twoDArray[row].length; column++){
+        twoDArray[row][column] = input.nextDouble();
+      }
+    }
+    
+    Location location1 = new Location(rows, columns);
+    double[]results = location1.locateLargest(twoDArray);
+    System.out.println("The location of the largest element is "+results[2]+" at ("+results[0]+", "+results[1]+")");
+  }
+
+
+}
