61923387ee51ce6fdecdcbdf9991633abd11d630
alexsmithmarist <alex.smith1@marist.edu>
2017-04-27 08:00:53 -0400

    variable sqrt no longer prints

--- labs/7/Lab7_problem1017.java
+++ labs/7/Lab7_problem1017.java
@@ -4,7 +4,7 @@
  * Author : Alex Smith
  * Course : CMPT220
  * Due Date : April 27, 2017
- * Version : 1.0
+ * Version : 1.1
  *
  * This file finds the first ten square numbers that are greater than Long.MAX_VALUE
  */
@@ -26,7 +26,7 @@ public class Lab7_problem1017{
     public static void findSquare(){
       double sqrt = Math.sqrt(Long.MAX_VALUE);
       sqrt = sqrt+1;
-      System.out.println(sqrt);
+      
       for(int i=0; i<10; i++){
         BigDecimal x = new BigDecimal(sqrt * sqrt);
         System.out.println(x);
